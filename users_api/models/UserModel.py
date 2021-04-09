08c636a6faaf3f6c2a2e1454fe1d0eafe56cbed9
carinaor <carinadel@pm.me>
2021-04-08 21:09:42 -0300

    User Model

--- users_api/models/UserModel.py
+++ users_api/models/UserModel.py
@@ -2,11 +2,11 @@ from users_api import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    firstname = db.Column(db.String(80), unique=True, nullable=False)
-    lastname = db.Column(db.String(80), unique=True, nullable=False)
+    firstname = db.Column(db.String(80), unique=False, nullable=False)
+    lastname = db.Column(db.String(80), unique=False, nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), unique=False, nullable=False)
 
     def __init__(self, firstname, lastname, username, email, password):
         self.firstname = firstname
